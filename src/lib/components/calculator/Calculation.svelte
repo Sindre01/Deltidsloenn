6d3d7b1f7127043b56a0fb786880d4927c9add64
Sindre Rotnes Erichsen <sindreerichsen@gmail.com>
2022-12-08 14:12:44 +0100

    ferdig med utregning

--- src/lib/components/calculator/Calculation.svelte
+++ src/lib/components/calculator/Calculation.svelte
@@ -1,48 +1,38 @@
 <script lang="ts">
   import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+  export let calculationsObj = {hourWage: 0, timesAndAdditions: [["", 0]], sumSalary: 0, sumAdditions: 0}
 </script>
 
 <div class= "main">
   <div>
-    <h2> Utregning </h2>
+    <h2> Time for time utregning </h2>
   </div>
   <DataTable table$aria-label="People list" style="max-width: 100%;">
     <Head>
       <Row>
-        <Cell>Kl</Cell>
+        <Cell>Time</Cell>
         <Cell numeric>lønn</Cell>
-     
         <Cell numeric>Tillegg</Cell>
-    
         <Cell numeric>Sum</Cell>
       </Row>
     </Head>
-    <Body>
-      <Row>
-        <Cell>16-17</Cell>
-        <Cell>160 kr</Cell>
-        <Cell numeric>45 kr</Cell>
-        <Cell numeric>205 kr</Cell>
-      </Row>
-      <Row>
-        <Cell>17-18</Cell>
-        <Cell>160 kr</Cell>
-        <Cell numeric>45 kr</Cell>
-        <Cell numeric>205 kr</Cell>
-      </Row>
-      <Row >
-          <Cell style="color: red;">Totalt: </Cell>
-          <Cell>360 kr</Cell>
-          <Cell>90 kr</Cell>
-          <Cell>410 kr</Cell>
-       
 
+    <Body>
+      {#each calculationsObj.timesAndAdditions as arr}
+      <Row>
+        <Cell>{arr[0]}</Cell>
+        <Cell numeric>{calculationsObj.hourWage} </Cell>
+        <Cell numeric>{arr[1]} </Cell>
+        <Cell numeric>{Number(calculationsObj.hourWage) + Number(arr[1])} kr</Cell>
       </Row>
-     
-    
-    
-      
-      
+      {/each}
+
+      <Row >
+          <Cell style="color: red; border-top: 1px solid red; ">Totalt: </Cell>
+          <Cell numeric style="border-top: 1px solid red; ">{calculationsObj.sumSalary} </Cell>
+          <Cell numeric style="border-top: 1px solid red; ">{calculationsObj.sumAdditions} </Cell>
+          <Cell numeric style="border-top: 1px solid red; ">{calculationsObj.sumSalary + calculationsObj.sumAdditions} kr</Cell>
+      </Row> 
     </Body>
     
   </DataTable>
--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -1,3 +1,4 @@
 import { writable } from "svelte/store";
 
-export let isMobile = writable();
+export let isMobile = writable(); 
+//export let calculationsObj = writable();
